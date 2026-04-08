cc3464f5565cbdec63510cd90f8fce60aa736dd9
atyab18 <mohd.atyab@cognizant.com>
2026-04-08 11:24:35 +0530

    first commit

---
+++ src/test/java/com/interns/saMBHAr/SaMbhArApplicationTests.java
@@ -0,0 +1,13 @@
+package com.interns.saMBHAr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaMbhArApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
